f2fe1f833c493e8bd92e320807ff9ceb117bed6e
Ervin <ean1207@gmail.com>
2021-02-28 15:16:59 -0800

    Setting up Scene

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
+// Components
+import { Canvas } from 'react-three-fiber';
+// Model
+import Model from './components/three/PyramidScene';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+        colorManagement
+        shadowMap
+        camera={{position:[0,0,120], fov: 70}}>
+          <Suspense fallback={null}>
+            <Model/>
+          </Suspense>
+    </Canvas>
   );
 }
 
